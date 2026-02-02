34c2e7f602886a62aa3725ec78ecdad9ad819d39
grkmtnkwebdevtry <gorkem.tanik1@gmail.com>
2026-02-02 16:45:16 +0000

    Pages-Childs dynamic WP

--- wp-content/themes/fictional-university-theme/footer.php
+++ wp-content/themes/fictional-university-theme/footer.php
@@ -1,9 +1,10 @@
-<footer class="site-footer">
+ <hr>
+ <footer class="site-footer">
       <div class="site-footer__inner container container--narrow">
         <div class="group">
           <div class="site-footer__col-one">
             <h1 class="school-logo-text school-logo-text--alt-color">
-              <a href="#"><strong>Fictional</strong> University</a>
+              <a href="<?php echo site_url() ?>"><strong>Fictional</strong> University</a>
             </h1>
             <p><a class="site-footer__link" href="#">555.555.5555</a></p>
           </div>
@@ -26,7 +27,7 @@
               <nav class="nav-list">
                 <ul>
                   <li><a href="#">Legal</a></li>
-                  <li><a href="#">Privacy</a></li>
+                  <li><a href="<?php echo site_url('/privacy-policy') ?> ">Privacy</a></li>
                   <li><a href="#">Careers</a></li>
                 </ul>
               </nav>
